147ea820b34c1ee5668fab2dd5a4567d6c71c837
ValentinaRod <v.rodriguez1425@gmail.com>
2017-08-22 22:28:22 -0300

    añadido funcionalidades basicas falta el llamado ajax

--- public/js/pantalla4.js
+++ public/js/pantalla4.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	//solo letras numeros @ y . en el input
-	$(document).on('keypress', '#email-input', function (event) {
+	$(document).on('keypress', '#email', function (event) {
 	    var regex = new RegExp("^[a-z0-9@.]+$");
 	    var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
 	    if (!regex.test(key)) {
@@ -9,8 +9,8 @@ $(document).ready(function() {
 	    }
 	});
 	//Solo numeros y máximo 8 caracteres en el input
-	$(document).on('keypress', '#password-input', function (event) {
-	    var regex = new RegExp("^[0-9]{1,8}$");
+	$(document).on('keypress', '#password', function (event) {
+	    var regex = new RegExp("^[0-6]{1,6}$");
 	    var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
 	    if (!regex.test(key)) {
 	        event.preventDefault();
@@ -18,9 +18,9 @@ $(document).ready(function() {
 	    }
 	});
 
-	$("#iniciar-sesion").on("click", function(e){
-	    var email = $("#email-input").val();
-	    var password = $("#password-input").val();
+	$("#btn_waves").on("click", function(e){
+	    var email = $("#email").val();
+	    var password = $("#password").val();
 
 	    if( email == "" || password == "" ){
 	        alert("No puede ingresar un campo vacío")
@@ -28,8 +28,8 @@ $(document).ready(function() {
 	    if( email.indexOf('@') == -1 ){
 	        alert("Ingrese un correo válido");
 	    }
-	    if( password.length > 8 ){
-	        alert("Su contraseña debe ser máximo 8 caracteres.");
+	    if( password.length > 6 ){
+	        alert("Su contraseña debe ser máximo 6 caracteres.");
 	        e.preventDefault();
 	    }
 	    if( password != "" && email.indexOf('@') != -1 ){
@@ -37,5 +37,29 @@ $(document).ready(function() {
 	    } else {
 	        e.preventDefault();
 	    }
-	});
-});
+		
+$.ajax({
+		url: '/createUser',
+		type: 'POST',
+		data: {
+			
+		},
+		})
+		.done(function(res) {
+			console.log("success");
+			console.log(res);
+			validPhone(res);
+		})
+		.fail(function() {
+			console.log("error");
+		})
+		.always(function() {
+			console.log("complete");
+		
+		});
+	
+
+
+
+		});//cierre boton crear cuenta.
+}); //cierre document.ready.
