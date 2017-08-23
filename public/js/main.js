2eb1a7437c96dec063f1cfa7ffdb959258ba3e07
Gilia Gongora <giliagongora.g@gmail.com>
2017-08-23 12:53:25 -0300

    Desarrollo pantalla3 y pantalla 5

--- public/js/main.js
+++ public/js/main.js
@@ -2,15 +2,5 @@ $(document).ready(function () {
 
 
 
-//funcion para el tiempo de pantalla 3
-var intervalo;
-function timer(){
-  intervalo = setInterval(nuevoCodigo, 21000);
-}
 
-function nuevoCodigo(){
-  alert(“Tu código ya no es válido, intenta pidiendo uno nuevo.“)
-}
-//fin pantalla3
-  
 });
